1e9247971a23299cb7c0e6f693b714a52d6657c0
David Hall <dlwh@berkeley.edu>
2009-10-11 00:52:25 -0700

    placeholder test code.

---
+++ src/test/scala/scalanlp/fst/Transducer.scala
@@ -0,0 +1,21 @@
+package scalanlp.fst;
+
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.FunSuite
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.Before
+
+
+import scalanlp.math._;
+import scala.collection.Traversable;
+import scala.collection.Seq;
+import scala.collection.mutable.PriorityQueue;
+
+
+@RunWith(classOf[JUnitRunner])
+class TransducerTest extends FunSuite {
+  import Transducer._;
+  test("test1") {
+  }
+}
