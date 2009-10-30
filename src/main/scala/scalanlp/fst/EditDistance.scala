3dafabd3635d93a47b61bfa8c8e4dba1852985e0
David Hall <dlwh@berkeley.edu>
2009-10-29 17:24:24 -0700

    Added a decay parameter to ED so that ED would enforce a joint distribution over alignments.

--- src/main/scala/scalanlp/fst/EditDistance.scala
+++ src/main/scala/scalanlp/fst/EditDistance.scala
@@ -7,23 +7,30 @@ import scalanlp.math.Semiring.LogSpace._;
 * alignment) with the given parameters, which must be less than 0.
 *
 * A match is assumed to be 0.0, though these will be rescaled to ensure
-* that the edit distance encodes a (log space) probability distribution.
+* that the edit distance encodes a (log space) probability distribution, or at
+* least that conditioned on either the input or the output, there is a distribution. 
+* logDecay (&lt;=0.0) adds a preference for the length of the alignments.
 * That is, the scores will be shifted by some constant
+*
+* 
+*
+* @author dlwh
 */
-class EditDistance( subRatio: Double, insRatio: Double, alphabet: Set[Char]) 
+class EditDistance( subRatio: Double, insRatio: Double, alphabet: Set[Char], logDecay: Double = 0.0) 
     extends Transducer[Double,Unit,Char,Char]()(doubleIsLogSpace,implicitly[Alphabet[Char]],implicitly[Alphabet[Char]]) {
   import Transducer._;
   require( subRatio < 0);
   require( insRatio < 0);
+  require( logDecay <= 0);
 
   val (insCost,subCost,matchCost) = {
     import Math.{exp,log};
     val n = alphabet.size
     // we have n^2-n subs, n matches, and 2n dels == insertions.
     // We want transitions to be markovian, so:
-    //  c * ((n^2 - n ) exp(sub) + n * exp(0) + 2n exp(del)) = 1
-    // log c + log ((n^2 - n ) exp(sub) + n * exp(0) + 2n exp(del)) = 0
-    val logC = - log( (n*n - n) * exp(subRatio) + n + 2 * n * exp(insRatio))
+    //  c * ((n^2 - n ) exp(sub) + n * exp(0) + 2n exp(del)) = decay
+    // log c + log ((n^2 - n ) exp(sub) + n * exp(0) + 2n exp(del)) = log(decay)
+    val logC = logDecay - log( (n*n - n) * exp(subRatio) + n + 2 * n * exp(insRatio))
     (insRatio + logC,subRatio + logC,logC)
   }
 
