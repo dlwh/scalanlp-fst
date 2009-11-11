7ba0f2c16671c5325ba2e3fea37f9405760dab0d
David Hall <dlwh@berkeley.edu>
2009-11-11 13:06:43 -0800

    Prune by state thresholding

--- src/main/scala/scalanlp/fst/Pruning.scala
+++ src/main/scala/scalanlp/fst/Pruning.scala
@@ -3,7 +3,7 @@ package scalanlp.fst;
 import scalanlp.math._;
 
 object Pruning {
-  def calculateStateFlow[W:Ordering,State,In,Out](auto: Transducer[W,State,In,Out]): Map[State,W] = {
+  def calculateStateFlow[W,State,In,Out](auto: Transducer[W,State,In,Out]): Map[State,W] = {
     val forward = auto.allPathDistances;
     val backward = auto.reverse.allPathDistances;
     val combined = for {
@@ -16,5 +16,21 @@ object Pruning {
     }
 
     combined withDefaultValue auto.ring.zero;
+  }
+
+  def prune[W,State,In,Out](auto: Transducer[W,State,In,Out], belowThreshold: W=>Boolean): Transducer[W,State,In,Out] = {
+    val fb = calculateStateFlow(auto);
+    val validStates = Set.empty ++ { for( (s,w) <- fb if !belowThreshold(w)) yield s }
+    val initWeights = auto.initialStateWeights.filter { case (k,v) => validStates(k) };
+    val newFinalWeights = auto.finalStateWeights.filter { case(k,v) => validStates(k) };
+
+    val newArcs = for( a@Arc(from,to,in,out,w) <- auto.allEdges
+      if validStates(from) && validStates(to) ) 
+      yield a;
+      
+    val imInitWeights = Map.empty ++ initWeights withDefaultValue(auto.ring.zero);
+    val imFinalWeights = Map.empty ++ newFinalWeights withDefaultValue(auto.ring.zero);
+    import auto._;
+    Transducer.transducer(imInitWeights,imFinalWeights)(newArcs:_*)(auto.ring,inAlpha,outAlpha);
   }
 }
