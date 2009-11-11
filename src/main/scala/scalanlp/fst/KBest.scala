2cef2fa6e62d21aca043039a6fb15d6b860c2c4a
David Hall <dlwh@berkeley.edu>
2009-11-11 13:45:02 -0800

    adds A* search to KBest list extraction

--- src/main/scala/scalanlp/fst/KBest.scala
+++ src/main/scala/scalanlp/fst/KBest.scala
@@ -45,29 +45,37 @@ object KBest {
   }
 
   def extractList[W:Ordering,State,T](auto: Automaton[W,State,T], num: Int):Seq[(Seq[T],W)] = {
-    case class Derivation(str: ArrayBuffer[T], state: State, weight: W, atFinal: Boolean);
-    implicit val orderDeriv = Ordering[W].on[Derivation](_.weight);
-    val kbest = new ArrayBuffer[(Seq[T],W)];
     import auto.ring._;
+    val heuristics = auto.reverse.allPathDistances;
+
+    case class Derivation(str: ArrayBuffer[T], state: State, weight: W, heuristic: W, atFinal: Boolean);
+    implicit val orderDeriv = Ordering[W].on[Derivation](_.heuristic);
+    val kbest = new ArrayBuffer[(Seq[T],W)];
+
 
     val pq = new PriorityQueue[Derivation];
     for( (state,w) <- auto.initialStateWeights) {
-      pq += Derivation(ArrayBuffer.empty,state,w,false);
+      pq += Derivation(ArrayBuffer.empty,state,w,times(w,heuristics(state)),false);
     }
 
     while(!pq.isEmpty && kbest.length < num) {
-      val deriv@Derivation(str,state,weight,atFinal) = pq.dequeue;
+      //println(pq.size);
+      val deriv@Derivation(str,state,weight,_,atFinal) = pq.dequeue;
       if(!atFinal) {
         val finalWeight = auto.finalWeight(state);
         if(finalWeight != zero) {
-          pq += Derivation(str,state,times(weight,finalWeight),true);
+          val finalScore = times(weight,finalWeight);
+          pq += Derivation(str,state,finalScore,finalScore,true);
         }
 
         for( Arc(_,to,ch,_,w) <- auto.edgesFrom(state)) {
           val newDeriv = if(ch == auto.inAlpha.epsilon) str else (str.clone() += ch)
-          pq += Derivation(newDeriv,to,times(weight,w),false);
+          val newWeight = times(weight,w);
+          val newHeuristic = times(newWeight,heuristics(to));
+          pq += Derivation(newDeriv,to,newWeight,newHeuristic,false);
         }
       } else {
+        //println("emit");
         kbest += ((deriv.str,deriv.weight));
       }
     }
