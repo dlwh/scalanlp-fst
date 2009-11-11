f68aa8e68f410e108c6189cb51bc358961c11475
David Hall <dlwh@berkeley.edu>
2009-11-11 13:06:00 -0800

    KL Minimize now minimizes!

--- src/main/scala/scalanlp/fst/KLMinimize.scala
+++ src/main/scala/scalanlp/fst/KLMinimize.scala
@@ -34,8 +34,7 @@ object KLMinimize {
       EquivalenceInfo(finalWeight(state),edgesWithoutStates)
     };
     val partitions = new ArrayBuffer[(EquivalenceInfo[In,Out],Set[State])];
-    partitions ++= initialPartitions;
-    printKLDivergences(partitions)
+    partitions ++= klMerge(initialPartitions.toSeq);
 
     val partitionOf:scala.collection.mutable.Map[State,Int] = makeMap(-1);
     for( (partition,index) <- partitions.zipWithIndex;
@@ -48,16 +47,17 @@ object KLMinimize {
       changed = false;
       for( ((oldEquiv,partition),index) <- partitions.zipWithIndex) {
         // try regrouping these edges by their follow sets
-        val newPartitions = partition.groupBy { state =>
+        val tryPartitions = partition.groupBy { state =>
           val myEdges = edgesByOrigin.getOrElse(state,Seq.empty).view;
           val edgesWithoutStates = aggregateArcs(myEdges,partitionOf );
           EquivalenceInfo(finalWeight(state),edgesWithoutStates);
         }
+        assert(tryPartitions.size > 0);
+
+        val newPartitions = klMerge(tryPartitions.toSeq);
 
         // if it changed, move the partitions
         if(newPartitions.size > 1) {
-          println("split");
-          printKLDivergences(newPartitions.toSeq);
           //println("split" + index + partition + newPartitions);
           changed = true;
           // leave one partition where it is.
@@ -126,6 +126,68 @@ object KLMinimize {
     intTransducer(imInitWeights,imFinalWeights)(arcs:_*);
   }
 
+  def computeKLDivergence[In,Out](p1: EquivalenceInfo[In,Out], p2: EquivalenceInfo[In,Out]) = {
+    val extraMass1 = p1.arcs.valuesIterator.toSeq.min/10;
+    val extraMass2 = p2.arcs.valuesIterator.toSeq.min/10;
+    val p1Total = logSum(p1.arcs.valuesIterator.toSeq);
+    val p2Total = logSum(p2.arcs.valuesIterator.toSeq);
+    val p1SmoothedTotal = logSum(logSum(p1.arcs.valuesIterator.toSeq),log(p1.arcs.size) +extraMass1);
+    val p2SmoothedTotal = logSum(logSum(p2.arcs.valuesIterator.toSeq),log(p2.arcs.size)  + extraMass2);
+
+    def lp1Smooth(w1: Double) = {
+      logSum(w1,extraMass1) - p1SmoothedTotal;
+    }
+    def lp2Smooth(w2: Double) = {
+      logSum(w2,extraMass2) - p2SmoothedTotal;
+    }
+    def lp1(w1: Double) = {
+      w1 - p1Total;
+    }
+    def lp2(w2: Double) = {
+      w2 - p2Total
+    }
+    var kl = 0.0;
+    var smoothedKL = 0.0;
+    for( (tuple,w1) <- p1.arcs;
+      w2 = p2.arcs.getOrElse(tuple,-1.0/0.0)
+    ) {
+      kl += Math.exp(lp1(w1)) * (lp1(w1) - lp2(w2));
+      smoothedKL += Math.exp(lp1Smooth(w1)) * (lp1Smooth(w1) - lp2Smooth(w2));
+    }
+
+    (kl,smoothedKL);
+  }
+
+
+  val ARBITRARY_CUTOFF = 1.0/0.0;
+
+  def klMerge[State,In,Out](partitions: Seq[(EquivalenceInfo[In,Out],Set[State])]) = {
+    val newPartitions = new ArrayBuffer[(EquivalenceInfo[In,Out],Set[State])];
+    newPartitions += partitions(0);
+    for( part1@(p1,states1) <- partitions) {
+
+      val (bestPartition:Int,score) = {
+        (for( ((p2,states2),index) <- newPartitions.iterator.zipWithIndex) yield {
+          val (kl,_) = computeKLDivergence(p1,p2);
+          val (rKl,_) = computeKLDivergence(p2,p1);
+          (index:Int,kl + rKl)
+        }).foldLeft( (-1,ARBITRARY_CUTOFF) ) ( (best,next) =>
+          if(next._2 <= best._2) next else best 
+        )
+      }
+
+      // either keep this partition or merge it with the other one.
+      if(score.isInfinite) {
+        newPartitions += part1;
+      } else {
+        val current = newPartitions(bestPartition);
+        newPartitions(bestPartition) =  (current._1,current._2 ++ states1);
+      }
+    }
+    newPartitions
+  }
+
+
   def printKLDivergences[State,In,Out](partitions: Seq[(EquivalenceInfo[In,Out],Set[State])]) = {
     for( (p1,states1) <- partitions;
       (p2,states2) <- partitions
@@ -133,39 +195,12 @@ object KLMinimize {
     ) {
       println(states1 + " cross " + states2);
       println(p1 + " vs. " + p2);
+      val (kl,smoothedKL) = computeKLDivergence(p1,p2);
 
-      val extraMass1 = p1.arcs.valuesIterator.toSeq.min/10;
-      val extraMass2 = p2.arcs.valuesIterator.toSeq.min/10;
-      val p1Total = logSum(p1.arcs.valuesIterator.toSeq);
-      val p2Total = logSum(p2.arcs.valuesIterator.toSeq);
-      val p1SmoothedTotal = logSum(logSum(p1.arcs.valuesIterator.toSeq),log(p1.arcs.size) +extraMass1);
-      val p2SmoothedTotal = logSum(logSum(p2.arcs.valuesIterator.toSeq),log(p2.arcs.size)  + extraMass2);
-
-      def lp1Smooth(w1: Double) = {
-        logSum(w1,extraMass1) - p1SmoothedTotal;
-      }
-      def lp2Smooth(w2: Double) = {
-        logSum(w2,extraMass2) - p2SmoothedTotal;
-      }
-      def lp1(w1: Double) = {
-        w1 - p1Total;
-      }
-      def lp2(w2: Double) = {
-        w2 - p2Total
-      }
-      var kl = 0.0;
-      var smoothedKL = 0.0;
-      for( (tuple,w1) <- p1.arcs;
-           w2 = p2.arcs.getOrElse(tuple,-1.0/0.0)
-         ) {
-         kl += Math.exp(lp1(w1)) * (lp1(w1) - lp2(w2));
-         smoothedKL += Math.exp(lp1Smooth(w1)) * (lp1Smooth(w1) - lp2Smooth(w2));
-      }
       println("Raw: " + kl + "  Smoothed: "+ smoothedKL);
       println();
       println();
     }
-
   }
 
 }
