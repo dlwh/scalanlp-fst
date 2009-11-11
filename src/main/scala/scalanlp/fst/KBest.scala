ed860bf201d4bb8314fc590977d10db09304d743
David Hall <dlwh@berkeley.edu>
2009-11-11 13:06:17 -0800

    hopefully sped up KBest

--- src/main/scala/scalanlp/fst/KBest.scala
+++ src/main/scala/scalanlp/fst/KBest.scala
@@ -1,19 +1,20 @@
 package scalanlp.fst;
 
 import scala.collection.mutable.PriorityQueue;
+import scala.collection.mutable.ArrayBuffer;
 
 import scalanlp.math._;
 
 object KBest {
   def extract[W:Ordering,State,T](auto: Automaton[W,State,T]):Iterator[(Seq[T],W)] = {
-    case class Derivation(reversedDeriv: List[T], state: State, weight: W, atFinal: Boolean);
+    case class Derivation(str: ArrayBuffer[T], state: State, weight: W, atFinal: Boolean);
     implicit val orderDeriv = Ordering[W].on[Derivation](_.weight);
 
     val derivations = new Iterator[Derivation] {
       import auto.ring._;
       val pq = new PriorityQueue[Derivation];
       for( (state,w) <- auto.initialStateWeights) {
-        pq += Derivation(Nil,state,w,false);
+        pq += Derivation(ArrayBuffer.empty[T],state,w,false);
       }
 
       def hasNext:Boolean = {
@@ -21,15 +22,15 @@ object KBest {
       }
 
       def next = {
-        val deriv@Derivation(rev,state,weight,atFinal) = pq.dequeue;
+        val deriv@Derivation(str,state,weight,atFinal) = pq.dequeue;
         if(!atFinal) {
           val finalWeight = auto.finalWeight(state);
           if(finalWeight != zero) {
-            pq += Derivation(rev,state,times(weight,finalWeight),true);
+            pq += Derivation(str,state,times(weight,finalWeight),true);
           }
 
           for( Arc(_,to,ch,_,w) <- auto.edgesFrom(state)) {
-            val newDeriv = if(ch == auto.inAlpha.epsilon) rev else ch :: rev;
+            val newDeriv = if(ch == auto.inAlpha.epsilon) str else (str.clone() += ch);
             pq += Derivation(newDeriv,to,times(weight,w),false);
           }
         }
@@ -38,7 +39,38 @@ object KBest {
     }
 
     val kbest = for( deriv <- derivations if deriv.atFinal)
-      yield (deriv.reversedDeriv.reverse,deriv.weight);
+      yield (deriv.str,deriv.weight);
+
+    kbest
+  }
+
+  def extractList[W:Ordering,State,T](auto: Automaton[W,State,T], num: Int):Seq[(Seq[T],W)] = {
+    case class Derivation(str: ArrayBuffer[T], state: State, weight: W, atFinal: Boolean);
+    implicit val orderDeriv = Ordering[W].on[Derivation](_.weight);
+    val kbest = new ArrayBuffer[(Seq[T],W)];
+    import auto.ring._;
+
+    val pq = new PriorityQueue[Derivation];
+    for( (state,w) <- auto.initialStateWeights) {
+      pq += Derivation(ArrayBuffer.empty,state,w,false);
+    }
+
+    while(!pq.isEmpty && kbest.length < num) {
+      val deriv@Derivation(str,state,weight,atFinal) = pq.dequeue;
+      if(!atFinal) {
+        val finalWeight = auto.finalWeight(state);
+        if(finalWeight != zero) {
+          pq += Derivation(str,state,times(weight,finalWeight),true);
+        }
+
+        for( Arc(_,to,ch,_,w) <- auto.edgesFrom(state)) {
+          val newDeriv = if(ch == auto.inAlpha.epsilon) str else (str.clone() += ch)
+          pq += Derivation(newDeriv,to,times(weight,w),false);
+        }
+      } else {
+        kbest += ((deriv.str,deriv.weight));
+      }
+    }
 
     kbest
   }
