83f79f198091b1026845feeddb6a871bd0ac4eff
David Hall <dlwh@berkeley.edu>
2010-01-19 12:04:50 -0800

    Automaton for Poisson distribution over lengths

---
+++ src/main/scala/scalanlp/fst/PoissonAutomaton.scala
@@ -0,0 +1,44 @@
+package scalanlp.fst;
+
+import scalanlp.math.Semiring.LogSpace._;
+import scalanlp.stats.sampling.Poisson;
+
+/**
+* Generates a poisson distribution over string lengths, and a uniform
+* distribution over individual characters, up to some maximum length.
+* Afterwards, it is a geometric distrbution.
+*
+* Uses log probabilities.
+*
+* @author dlwh
+*/
+class PoissonAutomaton(val expectedLength: Double,
+                       maxLength: Int, chars: Set[Char], geometricStoppingProb: Double = 0.9) extends Automaton[Double,Int,Char] {
+  require( expectedLength > 0);
+
+  private val poi = new Poisson(expectedLength);
+  private val arcCost = -Math.log(chars.size);
+  private val leftOverProb = scalanlp.math.Numerics.logDiff(0.0,poi.logCdf(maxLength-1));
+
+  val initialStateWeights = Map(0 -> 0.0);
+  def finalWeight(s: Int) = if(s < maxLength) poi.logProbabilityOf(s) else leftOverProb + Math.log(geometricStoppingProb);
+
+  def edgesMatching(s: Int, a: Char) = {
+    if(s < maxLength) {
+      if(a == alphabet.sigma) {
+        for(a <- chars.iterator) yield Arc(s,s+1,a,arcCost)
+      } else if(a != alphabet.epsilon)
+        Iterator.single(Arc(s,s+1,a,arcCost))
+      else Iterator.empty;
+    } else if(s == maxLength) {
+      val arcCost = leftOverProb + Math.log(1-geometricStoppingProb) - Math.log(chars.size);
+      if(a == alphabet.sigma) {
+        for(a <- chars.iterator) yield Arc(s,s,a,arcCost)
+      } else if(a != alphabet.epsilon)
+        Iterator.single(Arc(s,s,a,arcCost))
+      else Iterator.empty;
+
+    } else Iterator.empty;
+  }
+}
+
